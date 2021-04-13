bdfe72e1b59074d10a4c8e6c61faee2418bbb48a
yuan0624 <yrgna70266@gmail.com>
2021-04-13 15:24:43 +0800

    fuck

--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 def index(request):
 	myname = "Yuan"
+	data = [i for i in range(1,43)]
+	random.shuffle(data)
+	lotto_numbers = data[0:6]
+	special_number = data[6]
 	return render(request, 'index.html', locals())
 
 # Create your views here.
