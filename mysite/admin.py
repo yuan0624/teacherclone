95346cbfc6119e48e630a9e745f0652c16ff2fd0
yuan0624 <yrgna70266@gmail.com>
2021-05-24 21:48:24 +0800

    first commit

---
+++ mysite/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from mysite.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'body', 'pub_date')
+
+admin.site.register(Post, PostAdmin)
+
---
+++ mysite/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+from mysite.models import Post
+
+def index(request):
+	posts = Post.objects.all()
+	myname = "Yuan"
+	data = [i for i in range(1,43)]
+	random.shuffle(data)
+	lotto_numbers = data[0:6]
+	special_number = data[6]
+	return render(request, 'index.html', locals())
+
+def show(request, id):
+	try:
+		target = Post.objects.get(id=id)
+	except:
+		target = None
+	return render(request, "showpost.html", locals())
+# Create your views here.
